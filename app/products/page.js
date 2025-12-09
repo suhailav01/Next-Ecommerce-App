58f78bb4359839c34ea2e7b06150b70dcdc82deb
suhail <suhailav125@gmail.com>
2025-12-09 16:24:09 -0600

    ubdate product page

--- app/products/page.js
+++ app/products/page.js
@@ -4,53 +4,35 @@ import Link from "next/link";
 import styles from "../products/products.module.css";
 
 export default async function Products() {
-  try {
-    const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+  const res = await fetch("https://fakestoreapi.com/products", {
+    cache: "no-store",
+  });
 
-    const res = await fetch(`${baseURL}/api/products`, {
-      cache: "no-store",
-    });
+  const products = await res.json();
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch Products API");
-    }
-
-    const products = await res.json();
-
-    return (
-      <div style={{ marginTop: "150px" }} className={styles.container}>
-        {products?.map((p) => (
-          <div key={p.id} className={styles.card}>
-            <div className={styles.imageBox}>
-              <img className={styles.image} src={p.image} alt={p.title} />
-            </div>
-
-            <div className={styles.details}>
-              <h4 className={styles.title}>{p.title.slice(0, 16)}...</h4>
-              <p className={styles.category}>{p.category}</p>
-
-              <div className={styles.ratingBox}>
-                <span className={styles.rating}>{p.rating.rate} ⭐</span>
-                <span className={styles.count}>
-                  ({p.rating.count} reviews)
-                </span>
-              </div>
-
-              <Link href={`/id/${p.id}`} className={`btn ${styles.btn}`}>
-                View Details
-              </Link>
-            </div>
+  return (
+    <div style={{ marginTop: "150px" }} className={styles.container}>
+      {products?.map((p) => (
+        <div key={p.id} className={styles.card}>
+          <div className={styles.imageBox}>
+            <img className={styles.image} src={p.image} alt={p.title} />
           </div>
-        ))}
-      </div>
-    );
 
-  } catch (err) {
-    return (
-      <div style={{ marginTop: "150px", textAlign: "center", color: "red" }}>
-        <h2>Failed to load products 😔</h2>
-        <p>{err.message}</p>
-      </div>
-    );
-  }
+          <div className={styles.details}>
+            <h4 className={styles.title}>{p.title.slice(0, 16)}...</h4>
+            <p className={styles.category}>{p.category}</p>
+
+            <div className={styles.ratingBox}>
+              <span className={styles.rating}>{p.rating.rate} ⭐</span>
+              <span className={styles.count}>({p.rating.count} reviews)</span>
+            </div>
+
+            <Link href={`/id/${p.id}`} className={`btn ${styles.btn}`}>
+              View Details
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
